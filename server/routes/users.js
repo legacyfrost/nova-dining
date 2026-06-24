e035b4b33e92ca0a1aabffbbf3ba5b218ae7de43
legacyfrost <zacklee807@yahoo.com>
2026-06-24 18:18:08 +0300

    force gmail ipv4

--- server/routes/users.js
+++ server/routes/users.js
@@ -11,17 +11,21 @@ const dns = require("dns");
 dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  host:"smtp.gmail.com",
+  port:587,
+  secure:false,
 
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
+  connectionTimeout:10000,
+  greetingTimeout:10000,
+  socketTimeout:10000,
+
+  auth:{
+    user:process.env.EMAIL_USER,
+    pass:process.env.EMAIL_PASS
   },
 
-  tls: {
-    rejectUnauthorized: false
+  tls:{
+    rejectUnauthorized:false
   }
 });
 async function logActivity(userId,action){
