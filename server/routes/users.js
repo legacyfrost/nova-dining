10ecf250fc6938fd6f8a8f2d5495dabe33e67a44
legacyfrost <zacklee807@yahoo.com>
2026-06-24 18:05:32 +0300

    fix nodemailer smtp railway

--- server/routes/users.js
+++ server/routes/users.js
@@ -6,24 +6,16 @@ const jwt = require("jsonwebtoken");
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-
 host: "smtp.gmail.com",
 port: 587,
 secure: false,
-
-auth:{
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
+auth: {
+user: process.env.EMAIL_USER,
+pass: process.env.EMAIL_PASS
 },
-
-tls:{
-    rejectUnauthorized:false
-},
-
-connectionTimeout: 10000,
-greetingTimeout: 10000,
-socketTimeout: 10000
-
+tls: {
+rejectUnauthorized: false
+}
 });
 
 async function logActivity(userId,action){
